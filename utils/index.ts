d7c4312c34570c4404680efdd932d2e381cbb78b
Otto Rahn <elias.yunusoff@gmail.com>
2023-11-17 22:34:24 +0500

    api was added

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { CarProps } from '@/types'
+
 export async function fetchCars() {
   const headers = {
     'X-RapidAPI-Key': '5b8348fe47mshc06b48f5f015ee2p195b27jsnbadd3cf185ce',
@@ -27,4 +29,20 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   const rentalRatePerDay = basePricePerDay + mileageRate + ageRate
 
   return rentalRatePerDay.toFixed(0)
+}
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL('https://cdn.imagin.studio/getimage')
+
+  const { make, year, model } = car
+
+  url.searchParams.append('customer', 'hrjavascript-mastery')
+
+  url.searchParams.append('make', make)
+  url.searchParams.append('modelFamily', model.split(' ')[0])
+  url.searchParams.append('zoomType', 'fullscreen')
+  url.searchParams.append('modelYear', `${year}`)
+  url.searchParams.append('angle', `${angle}`)
+
+  return `${url}`
 }
